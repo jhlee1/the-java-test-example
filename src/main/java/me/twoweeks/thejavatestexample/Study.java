702add0cd7d420880e9332adcf9138fbccd9e0fa
Joohan Lee <joolee@ogqcorp.com>
2020-06-01 01:33:40 +0900

    Watched test iteration

--- src/main/java/me/twoweeks/thejavatestexample/Study.java
+++ src/main/java/me/twoweeks/thejavatestexample/Study.java
@@ -7,6 +7,7 @@ public class Study {
 
   private StudyStatus studyStatus;
   private int limit;
+  private String name;
 
   public StudyStatus getStudyStatus() {
     return studyStatus;
@@ -16,10 +17,29 @@ public class Study {
     return limit;
   }
 
+  public String getName() {
+    return name;
+  }
+
+  public Study(int limit, String name) {
+    this.limit = limit;
+    this.name = name;
+  }
+
   public Study(int limit) {
     if (limit < 0) {
       throw new IllegalArgumentException("Limit은 0보다 커야한다.");
     }
     this.limit = limit;
+  }
+
+
+  @Override
+  public String toString() {
+    return "Study{" +
+        "studyStatus=" + studyStatus +
+        ", limit=" + limit +
+        ", name='" + name + '\'' +
+        '}';
   }
 }
--- src/test/java/me/twoweeks/thejavatestexample/StudyTest.java
+++ src/test/java/me/twoweeks/thejavatestexample/StudyTest.java
@@ -23,14 +23,24 @@ import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledOnJre;
-import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.JRE;
 import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.test.annotation.Repeat;
 
 /**
  * Created by Joohan Lee on 2020/03/27
@@ -128,7 +138,7 @@ class StudyTest {
   void tag_example_custom_meta_tag() {
     assertThrows(IllegalArgumentException.class, () -> new Study(-10)); //
   }
-  
+
   // 테스트 반복하기
   // RepetitionInfo를 통해 여태 몇번 반복했는지, 몇번 반복해야 하는지를 알 수 있음
   @DisplayName("스터디만들기 반복")
@@ -136,7 +146,7 @@ class StudyTest {
   void repeat_example(RepetitionInfo repetitionInfo) {
     System.out.println("test " + repetitionInfo.getCurrentRepetition() + " / " + repetitionInfo.getTotalRepetitions());
   }
-  
+
   // 반복 테스트에서 각각 다른 Parameter를 넣어서 테스트해보기
   @ParameterizedTest
   @ValueSource(strings = {"The", "winter", "is", "coming"})
@@ -147,10 +157,60 @@ class StudyTest {
   // 반복 테스트에서 각각 다른 Parameter를 넣어서 테스트해보기 (이름 설정)
   @DisplayName("스터디만들기")
   @ParameterizedTest(name = "{index} {displayName} message={0}")
-  @ValueSource(strings = {"The", "winter", "is", "coming"})
+  @ValueSource(strings = {"The", "winter", "is", "coming"}) //strings 말고 longs, booleans 등 다양한 타입을 넣어줄 수 있음
+  @EmptySource // 위에 Value source에 더하여 빈 문자열을 넣어줌
+  @NullSource // 위에 Value source에 더하여 null을 넣어줌
+  @NullAndEmptySource //위에 두개 합친 composite annotation
   void parameterizedTestWithName(String message) {
     System.out.println(message);
   }
+
+  @DisplayName("Converter 써보기")
+  @ParameterizedTest(name = "{index} {displayName} message={0}")
+  @ValueSource(ints = {10, 20, 40})
+  void parameterizedTestWithConverter(@ConvertWith(StudyConverter.class) Study study) {
+    System.out.println(study.getLimit());
+  }
+
+  static class StudyConverter extends SimpleArgumentConverter {
+    @Override
+    protected Object convert(Object o, Class<?> aClass) throws ArgumentConversionException {
+      assertEquals(Study.class, aClass, "Can only convert to Study");
+      return new Study(Integer.parseInt(o.toString()));
+    }
+  }
+
+  @DisplayName("Csv 써보기")
+  @ParameterizedTest(name = "{index} {displayName} message={0}")
+  @CsvSource({"10, '자바 스터디'", "20, 스프링"})
+  void parameterizedTestWithCsv(Integer limit, String name) { // 위에 컨버터를 쓰는 방식은 argument 하나밖에 받지 못함
+    System.out.println(new Study(limit, name));
+  }
+
+  @DisplayName("ArgumentsAccessor")
+  @ParameterizedTest(name = "{index} {displayName} message={0}")
+  @CsvSource({"10, '자바 스터디'", "20, 스프링"})
+  void parameterizedTestWithArgumentsAccessor(ArgumentsAccessor argumentsAccessor) { // 위에 컨버터를 쓰는 방식은 argument 하나밖에 받지 못함
+    Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+    System.out.println(study);
+  }
+
+  @DisplayName("Aggregator")
+  @ParameterizedTest(name = "{index} {displayName} message={0}")
+  @CsvSource({"10, '자바 스터디'", "20, 스프링"})
+  void parameterizedTestWithAggregator(@AggregateWith(StudyAggregator.class) Study study) { // 위에 컨버터를 쓰는 방식은 argument 하나밖에 받지 못함
+    System.out.println(study);
+  }
+
+  static class StudyAggregator implements ArgumentsAggregator { // 반드시 static한 inner class 이거나 public class여야 한다.
+
+    @Override
+    public Object aggregateArguments(ArgumentsAccessor argumentsAccessor,
+        ParameterContext parameterContext) throws ArgumentsAggregationException {
+      return new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+    }
+  }
+
 
   // 모든 @Test들을 실행하기 전에 딱 한번만 사용됨. static으로 선언해야됨
   @BeforeAll
