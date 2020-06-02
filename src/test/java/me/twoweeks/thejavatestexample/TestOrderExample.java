304c3c189e60a9e6585ae3a2a5b3b33e634bcb4f
Joohan Lee <joolee@ogqcorp.com>
2020-06-02 22:54:54 +0900

    Finished the lecture about setting properties file

--- src/test/java/me/twoweeks/thejavatestexample/TestOrderExample.java
+++ src/test/java/me/twoweeks/thejavatestexample/TestOrderExample.java
@@ -4,6 +4,8 @@ package me.twoweeks.thejavatestexample;
 // 서로 간에 의존성이 존재하면 각 단위로써 존재하는 의미가 사라짐
 // 상황에 따라서 로그인 -> 액션 -> 로그아웃 이런 식으로 쭉 연결된 테스트가 필요할 때가 있음
 
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
